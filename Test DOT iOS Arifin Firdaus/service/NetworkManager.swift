228e8478be6fca4045013aa5cba6673b4f1aacab
Arifin <arifin.firdaus1996@gmail.com>
2018-03-16 08:51:25 +0700

    done fetch

---
+++ Test DOT iOS Arifin Firdaus/service/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  Test DOT iOS Arifin Firdaus
+//
+//  Created by Arifin Firdaus on 3/16/18.
+//  Copyright © 2018 Arifin Firdaus. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkManager {
+    
+    static let PostsUrl = "https://jsonplaceholder.typicode.com/posts"
+    
+}
