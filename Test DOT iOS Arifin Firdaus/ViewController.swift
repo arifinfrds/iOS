0b97d5e9cd5a5dc01588225563d33f1d32544f79
Arifin <arifin.firdaus1996@gmail.com>
2018-03-16 08:00:09 +0700

    add base project

---
+++ Test DOT iOS Arifin Firdaus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test DOT iOS Arifin Firdaus
+//
+//  Created by Arifin Firdaus on 3/16/18.
+//  Copyright © 2018 Arifin Firdaus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
