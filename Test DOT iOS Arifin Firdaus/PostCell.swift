478b6235e2adcae3008774b9a7a794107a354f53
Arifin <arifin.firdaus1996@gmail.com>
2018-03-16 08:22:37 +0700

    done base ui post

---
+++ Test DOT iOS Arifin Firdaus/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Test DOT iOS Arifin Firdaus
+//
+//  Created by Arifin Firdaus on 3/16/18.
+//  Copyright © 2018 Arifin Firdaus. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    // MARK: - Properties
+    @IBOutlet weak var userIdLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+    
+    // MARK: - View life cycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
